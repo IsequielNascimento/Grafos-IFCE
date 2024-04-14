9e426b6c08b2ef67ca2141a6a962e8d1e7f59895
IsequielNascimento <isequielnascimento@gmail.com>
2024-04-14 17:24:09 -0300

    Inserting input limits

--- 1765-Itinerary-of-Santa-Claus.py
+++ 1765-Itinerary-of-Santa-Claus.py
@@ -20,12 +20,19 @@ while True:
     m, n = map(int, input().split())
     if m == 0 and n == 0:
         break
+    
+    if not (2 <= m < 40000 and 1 <= n < 50000):
+        print("Os valores de M e N estão fora do intervalo permitido.")
+        continue
 
     g = []
     p = [i for i in range(m + 1)]
 
     for _ in range(n):
         u, v, w = map(int, input().split())
+        if not (0 <= u < m and 0 <= v < m and 1 <= w <= 999):
+            print("Os valores de X, Y e Z estão fora do intervalo permitido.")
+            continue
         g.append((u, v, w))
 
     g.sort(key=lambda x: x[2])
